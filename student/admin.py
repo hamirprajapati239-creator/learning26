a4443435d9f9804954c06ae2686b203e1077fbd2
Hamir Prajapati <hamirprajapati239@gmail.com>
2026-02-04 12:32:50 +0530

    ORM

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student,product,fooditem
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(product)
+admin.site.register(fooditem)
--- student/models.py
+++ student/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    StudentName= models.CharField(max_length=100)
+    studentAge=models.IntegerField()
+    studentcity=models.CharField(max_length=40)
+    studentEmail=models.EmailField(null=True)
+
+    class Meta:
+        db_table = "student"
+
+class product(models.Model):
+    productName=models.CharField(max_length=100)
+    productPrice=models.IntegerField()
+    productDescription=models.TextField()
+    productstock=models.PositiveBigIntegerField()
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+    
+    
+    class Meta:
+        db_table = "product"
+
+class fooditem(models.Model):
+    foodName = models.CharField(max_length=100)
+    foodPrice = models.IntegerField()
+    foodDescription = models.TextField()
+    foodStock = models.PositiveBigIntegerField()
+    foodCategory = models.CharField(max_length=20, null=True)
+    foodStatus = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "fooditem"
